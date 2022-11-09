bfac54189c815343813739f9fd94bd5f4995d9e0
DevK1503 <117856118+DevK1503@users.noreply.github.com>
2022-11-10 04:48:32 +0530

    Add files via upload

---
+++ BorderlessRenderer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Views;
+using Android.Widget;
+using MyFirstApp.customrenderer;
+using MyFirstApp.Droid.renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(BorderlessEntry), typeof(BorderlessRenderer))]
+
+namespace MyFirstApp.Droid.renderers
+{
+    public class BorderlessRenderer:EntryRenderer
+    {
+        public BorderlessRenderer(Context context):base(context)
+        {
+             
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Android.Graphics.Color entryLineColor = Android.Graphics.Color.Transparent;
+                Control.BackgroundTintList = ColorStateList.ValueOf(entryLineColor);
+
+                
+            }
+        }
+    }
+}
---
+++ sqlconnection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MyFirstApp.datas;
+using SQLite;
+using Xamarin.Forms;
+using System.Threading;
+using Android.Webkit;
+
+
+
+[assembly: Dependency(typeof(MyFirstApp.Droid.connection.sqlconnection))]
+namespace MyFirstApp.Droid.connection
+{
+    public class sqlconnection : Idatabase
+    {
+        public SQLiteConnection GetConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SQLiteConnection GetSqlConnection()
+        {
+            var filename = "Places.db";
+            var documentspath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var path = System.IO.Path.Combine(documentspath, filename);
+            var connection = new SQLiteConnection(path);
+            return connection;
+            
+        }
+
+        public SQLiteConnection GetSqlconnection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
